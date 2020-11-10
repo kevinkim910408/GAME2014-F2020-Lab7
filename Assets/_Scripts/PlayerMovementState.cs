6d779399fac6155190c6c4ab1f9225be0050ba8e
kevinkim910408 <kevinkim0408@naver.com>
2020-11-11 00:29:56 +0900

    Initial commit

---
+++ Assets/_Scripts/PlayerMovementState.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[System.Serializable]
+public enum PlayerMovementState
+{
+    IDLE, // 0
+    RUN,
+    JUMP,
+    CROUCH,
+}
